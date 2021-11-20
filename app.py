d5b2e335725e96d144dc87d437c8c3e0d6e1c827
Manas0901 <94740688+Manas0901@users.noreply.github.com>
2021-11-20 16:23:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+from datetime import date
+
+# importing the random module
+import random
+
+
+# import Flask ::: for implementig flask server
+from flask import Flask, render_template,request
+
+
+# create a new web-app whixh is a Flask application while __name__	represents that his is  
+#the file that will reresent the web-app		
+app = Flask(__name__,static_url_path='/static')
+
+#import .jason files => hotels.json
+import json
+with open('jokes.json') as data_file1:    
+		data1 = json.load(data_file1)
+		#quote = data1['quotes'] # gor a list 
+
+#GLOBAL-variables => check_in, check_out.
+
+
+#/ represent the default page llocalhost/
+@app.route("/")
+#Declarator funtion
+def index():
+	today = date.today() 
+	rand = random.randint(0,24)
+	print(rand)
+
+	print(data1[rand])
+		#print(quote[rand]['author'])
+	#return json.dumps(data1[1])
+	return render_template("jokes.html",jokes=data1[rand], rand = rand , today = today)
+
+@app.route("/aman",methods=["get"])
+def aman():
+	return render_template("me.html")
+
+
+if __name__ == '__main__':
+	app.run()  
